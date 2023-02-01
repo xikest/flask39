ff68095aeb4d800121d1e055a50d6087278c19b3
TJ K <xikest12@gmail.com>
2023-02-01 13:14:34 +0000

    .

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'pybo.db')}"
+
